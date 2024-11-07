3bd75b84f0e60d2bb9c044933e6bc0ba42bcddfc
pyerre237 <pguitet5@gmail.com>
2024-11-07 10:30:08 +0100

    Add files via upload

--- tp6/test.js
+++ tp6/test.js
@@ -1,13 +1,12 @@
 function login(){
     var login = document.getElementById("t1").value;
-    var passworld = document.getAnimations("h1").value;
+    var passworld = document.getElementById("h1").value;
     if(login == "admin" && passworld == "admin")
- {
-
-window.location.href='yes.html'
- }
- else {
-    alert("Identifiants non reconnus");
-    window.location.href='non.html';
- }
+    {
+        window.location.href='yes.html';
+    }
+    else {
+        alert("Identifiants non reconnus");
+        window.location.href='non.html';
+    }
 }
